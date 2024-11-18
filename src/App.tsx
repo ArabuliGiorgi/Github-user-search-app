b314284806803990c5bbf52adfcc9475abc03337
ArabuliGiorgi <qartveli.giorgi.2003@gmail.com>
2024-11-18 17:17:24 +0400

    first steps & put themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { useState } from "react"
 import { Main } from "./styled-components/AppStyle"
+import Sun from "/images/icon-sun.svg"
+import Moon from "/images/icon-moon.svg"
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
-    <Main>
-      
+    <Main darkmode={`${darkMode}`}>
+      <div className="w-[100%] max-w-[730px]">
+        <div className="flex justify-between items-center mb-[36px]" id="header">
+          <h1 className="logo">devfinder</h1>
+          <div className="dark-mode" onClick={() => setDarkMode(!darkMode)}>
+            <h1>{darkMode ? "LIGHT" : "DARK"}</h1>
+            <img src={darkMode ? Sun : Moon} alt={darkMode ? "Sun" : "Moon"}/>
+          </div>
+        </div>
+        
+      </div>
     </Main>
   )
 }
--- src/styled-components/AppStyle.tsx
+++ src/styled-components/AppStyle.tsx
@@ -1,7 +1,39 @@
 import styled from "styled-components";
 
-const Main = styled.div`
-    
+const Main = styled.div<{darkmode: string}>`
+    width: 100%;
+    min-height: 100vh;
+    background-color: ${(props) => props.darkmode === "true" ? '#141D2F' : '#F6F8FF'};
+    padding: 31px 24px 79px;
+    display: flex;
+    align-items: start;
+    justify-content: center;
+
+    .logo{
+        font-size: 26px;
+        font-weight: 700;
+        line-height: 38.51px;
+        text-align: left;
+        color: ${(props) => props.darkmode === "true" ? '#FFFFFF' : '#222731'};
+    }
+    .dark-mode{
+        display: flex;
+        align-items: center;
+        gap: 16px;
+        cursor: pointer;
+
+        h1{
+            font-size: 13px;
+            font-weight: 700;
+            line-height: 19.25px;
+            letter-spacing: 2.5px;
+            text-align: right;
+            color: ${(props) => props.darkmode === "true" ? '#FFFFFF' : '#4B6A9B'};
+        }
+        img{
+            height: 20px;
+        }
+    }
 `
 
 export {Main}
